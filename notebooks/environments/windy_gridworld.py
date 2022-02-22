27c991eaa93391ea0e8bf4f112e7d0fd76e9c83a
marti <marti.sanchez@upf.edu>
2022-02-22 13:00:09 +0100

    Fixed some environment problems with grid_shape

--- notebooks/environments/windy_gridworld.py
+++ notebooks/environments/windy_gridworld.py
@@ -13,34 +13,34 @@ class WindyGridworldEnv(discrete.DiscreteEnv):
     metadata = {'render.modes': ['human', 'ansi']}
 
     def _limit_coordinates(self, coord):
-        coord[0] = min(coord[0], self.shape[0] - 1)
+        coord[0] = min(coord[0], self.grid_shape[0] - 1)
         coord[0] = max(coord[0], 0)
-        coord[1] = min(coord[1], self.shape[1] - 1)
+        coord[1] = min(coord[1], self.grid_shape[1] - 1)
         coord[1] = max(coord[1], 0)
         return coord
 
     def _calculate_transition_prob(self, current, delta, winds):
         new_position = np.array(current) + np.array(delta) + np.array([-1, 0]) * winds[tuple(current)]
         new_position = self._limit_coordinates(new_position).astype(int)
-        new_state = np.ravel_multi_index(tuple(new_position), self.shape)
+        new_state = np.ravel_multi_index(tuple(new_position), self.grid_shape)
         is_done = tuple(new_position) == (3, 7)
         return [(1.0, new_state, -1.0, is_done)]
 
     def __init__(self):
-        self.shape = (7, 10)
+        self.grid_shape = (7, 10)
 
-        nS = np.prod(self.shape)
+        nS = np.prod(self.grid_shape)
         nA = 4
 
         # Wind strength
-        winds = np.zeros(self.shape)
+        winds = np.zeros(self.grid_shape)
         winds[:,[3,4,5,8]] = 1
         winds[:,[6,7]] = 2
 
         # Calculate transition probabilities
         P = {}
         for s in range(nS):
-            position = np.unravel_index(s, self.shape)
+            position = np.unravel_index(s, self.grid_shape)
             P[s] = { a : [] for a in range(nA) }
             P[s][UP] = self._calculate_transition_prob(position, [-1, 0], winds)
             P[s][RIGHT] = self._calculate_transition_prob(position, [0, 1], winds)
@@ -49,7 +49,7 @@ class WindyGridworldEnv(discrete.DiscreteEnv):
 
         # We always start in state (3, 0)
         isd = np.zeros(nS)
-        isd[np.ravel_multi_index((3,0), self.shape)] = 1.0
+        isd[np.ravel_multi_index((3,0), self.grid_shape)] = 1.0
 
         super(WindyGridworldEnv, self).__init__(nS, nA, P, isd)
 
@@ -63,7 +63,7 @@ class WindyGridworldEnv(discrete.DiscreteEnv):
         outfile = StringIO() if mode == 'ansi' else sys.stdout
 
         for s in range(self.nS):
-            position = np.unravel_index(s, self.shape)
+            position = np.unravel_index(s, self.grid_shape)
             # print(self.s)
             if self.s == s:
                 output = " x "
@@ -74,7 +74,7 @@ class WindyGridworldEnv(discrete.DiscreteEnv):
 
             if position[1] == 0:
                 output = output.lstrip()
-            if position[1] == self.shape[1] - 1:
+            if position[1] == self.grid_shape[1] - 1:
                 output = output.rstrip()
                 output += "\n"
 

2e013c35d4392a849f29aab8eebfb745a7526778
marti <marti.sanchez@upf.edu>
2023-03-09 07:53:15 +0100

    added requirements and extended stableBaseLines code

--- other-rl-libraries/stableBaseLines3.py
+++ other-rl-libraries/stableBaseLines3.py
@@ -8,21 +8,53 @@
 # install:
 # pip install "stable-baselines3[extra]"
 
+import sys
 import gym
 
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, DQN
 
-env = gym.make("CartPole-v1")
+env_names = ["FrozenLake-v0", "CartPole-v1", "LunarLanderContinuous-v2", "LunarLander-v2"]
+
+env_name = "CartPole-v1" 
+
+if len(sys.argv) > 1:
+    i = int(sys.argv[1])
+    if i >= 0:
+        env_name = env_names[i] 
+    else:
+        env_name = sys.argv[1]
+ 
+env = gym.make(env_name) if env_name not in ["FrozenLake-v0", "FrozenLake-v1"] else gym.make(env_name, is_slippery=False)
+
+
+n_episodes = 10000
+if len(sys.argv) > 2:
+    i = int(sys.argv[2])
+    if i >= 0:
+        n_episodes = i
+
 
 model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10000)
+#model = DQN("MlpPolicy", env, verbose=1)
 
+model.learn(total_timesteps=n_episodes)
+model.save(env_name)
+
+
+total_reward = 0
+trial_reward = 0
 obs = env.reset()
 for i in range(1000):
     action, _states = model.predict(obs, deterministic=True)
     obs, reward, done, info = env.step(action)
-    env.render()
+    #env.render()
+
+    trial_reward += reward
     if done:
+      print("trial_reward", trial_reward)
+      total_reward += trial_reward
+      trial_reward = 0
       obs = env.reset()
 
+print("----- total_reward", total_reward)
 env.close()
